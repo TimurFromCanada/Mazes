555db14a7859ee212c24de1a1e991d2ca6af46e4
timurfromcanada <timurfromcanada@gmail.com>
2021-04-10 20:35:39 +0500

    Implemented EmptyMazeTask.cs

---
+++ EmptyMazeTask.cs
@@ -0,0 +1,25 @@
+﻿namespace Mazes
+{
+	public static class EmptyMazeTask
+	{
+		public static void MoveRight(Robot robot, int width, int height)
+		{
+			if (width > 2)
+				for (var i = 0; i < width - 3; i++)
+					robot.MoveTo(Direction.Right);
+		}
+
+		public static void MoveDown(Robot robot, int width, int height)
+		{
+			if (height > 2)
+				for (var i = 0; i < height - 3; i++)
+					robot.MoveTo(Direction.Down);
+		}
+
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			MoveRight(robot, width, height);
+			MoveDown(robot, width, height);
+		}
+	}
+}
