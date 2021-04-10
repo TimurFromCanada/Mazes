9d761b3548a6270e35de302d615b3848b93df937
timurfromcanada <timurfromcanada@gmail.com>
2021-04-10 20:49:00 +0500

    Implemented SnakeMazeTask.cs

---
+++ SnakeMazeTask.cs
@@ -0,0 +1,42 @@
+﻿namespace Mazes
+{
+    public static class SnakeMazeTask
+    {
+        public static void MoveRightAndDown(Robot robot, int width)
+        {
+            for (var i = 0; i < width - 3; i++)
+                robot.MoveTo(Direction.Right);
+
+            for (var i = 0; i < 2; i++)
+                robot.MoveTo(Direction.Down);
+        }
+
+        public static void MoveLeftAndDown(Robot robot, int width)
+        {
+            for (var i = 0; i < width - 3; i++)
+                robot.MoveTo(Direction.Left);
+
+            for (var i = 0; i < 2; i++)
+                robot.MoveTo(Direction.Down);
+        }
+
+        public static void MoveLeft(Robot robot, int width)
+        {
+            for (var i = 0; i < width - 3; i++)
+                robot.MoveTo(Direction.Left);
+        }
+
+        public static void MoveOut(Robot robot, int width, int height)
+        {
+            for (var i = 0; i < height / 2 - 1; i++)
+            {
+                if (i % 2 == 0)
+                    MoveRightAndDown(robot, width);
+
+                if (i % 2 != 0)
+                    MoveLeftAndDown(robot, width);
+            }
+            MoveLeft(robot, width);
+        }
+    }
+}
