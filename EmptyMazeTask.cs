65dadba20ffb8afdc6c1d39bd73ead39b6cb4fa2
timurfromcanada <timurfromcanada@gmail.com>
2021-04-10 20:43:54 +0500

    Implemented EmptyMazeTask.cs

--- EmptyMazeTask.cs
+++ EmptyMazeTask.cs
@@ -5,15 +5,23 @@
 		public static void MoveRight(Robot robot, int width, int height)
 		{
 			if (width > 2)
+            {
 				for (var i = 0; i < width - 3; i++)
+                {
 					robot.MoveTo(Direction.Right);
+				}
+			}
 		}
 
 		public static void MoveDown(Robot robot, int width, int height)
 		{
 			if (height > 2)
-				for (var i = 0; i < height - 3; i++)
+            {
+				for(var i = 0; i < height - 3; i++)
+                {
 					robot.MoveTo(Direction.Down);
+				}
+			}
 		}
 
 		public static void MoveOut(Robot robot, int width, int height)
